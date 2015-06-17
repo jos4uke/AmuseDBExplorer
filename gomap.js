8cb19813d2afe9a2a84f9cef39c2637b7127e519
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2015-06-17 13:48:25 +0200

    get the spot coordinates on the map

--- gomap.js
+++ gomap.js
@@ -1,15 +1,18 @@
-// When locator icon in datatable is clicked, go to that spot on the map
-$(document).on("click", ".go-map", function(e) {
-  e.preventDefault();
-  $el = $(this);
-  var lat = $el.data("lat");
-  var long = $el.data("long");
-  var av = $el.data("av");
-  $($("#nav a")[0]).tab("show");
-  Shiny.onInputChange("goto", {
-    lat: lat,
-    lng: long,
-    av: av,
-    nonce: Math.random()
+  // When locator icon in datatable is clicked, go to that spot on the map
+  $(document).on("click", ".go-map", function(e) {
+    e.preventDefault();
+    $el = $(this);
+    var lat = $el.data("lat");
+    var long = $el.data("long");
+    var av = $el.data("av");
+    var name = $el.data("name")
+    $($("#nav a")[0]).tab("show");
+    Shiny.onInputChange("goto", {
+      lat: lat,
+      lng: long,
+      av: av,
+      name: name,
+      nonce: Math.random()
+    });
   });
 });
